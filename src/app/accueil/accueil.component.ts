24a856896150f36b929320521e9d1e688a03143b
n2sy <nidhal.jelassi@fsegt.utm.tn>
2025-04-08 13:40:05 +0100

    navigation entre routes

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
 export class AccueilComponent {
-
+ private router = inject(Router);
+    
+    goToCv() {
+        this.router.navigateByUrl("cv")
+    }
+    
+    goToProducts() {
+        this.router.navigate(["products"])
+    }
 }
