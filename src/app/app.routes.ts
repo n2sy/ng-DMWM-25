3a8a6d907c1dc44cc63d7b08ee8be1168e1578f3
n2sy <nidhal.jelassi@fsegt.utm.tn>
2025-04-08 14:10:22 +0100

    snapshot vs observable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { CvComponent } from './cv/cv.component';
 import { HomeAccountComponent } from './accounts/home-account/home-account.component';
 import { HomeProductsComponent } from './products/home-products/home-products.component';
+import { InfosComponent } from './infos/infos.component';
 
 export const myRoutes: Routes = [
     {path : '', component: AccueilComponent },
     {path : 'cv', component: CvComponent },
+    {path : 'cv/:id', component: InfosComponent },
     {path : 'accounts', component: HomeAccountComponent },
     {path : 'products', component: HomeProductsComponent }
 ];
---
+++ src/app/infos/infos.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-infos',
+  imports: [RouterLink],
+  templateUrl: './infos.component.html',
+  styleUrl: './infos.component.css'
+})
+export class InfosComponent {
+    selectedId;
+    private activatedRoute = inject(ActivatedRoute);
+    
+    ngOnInit() {
+        //1ere manière
+        // console.log(this.activatedRoute.snapshot.params);
+        this.selectedId = this.activatedRoute.snapshot.paramMap.get('id');
+        
+        //2eme manière
+        this.activatedRoute.paramMap.subscribe(
+            {
+                next : (p : ParamMap) => {
+                    this.selectedId = p.get('id');
+                },
+                // error : ,
+                // complete : 
+            }
+        )
+        
+    }
+    
+
+}
